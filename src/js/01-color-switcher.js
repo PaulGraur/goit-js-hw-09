7182265e7065d3a2f127106f04f90bedc1824279
Paul <pawel.graur47@gmail.com>
2023-02-21 15:46:07 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const CHANGE_COLOR = 1000;
+let idInteger = null;
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+}
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartChangeColor);
+refs.btnStop.addEventListener('click', onBtnStopChangeColor);
+
+function onBtnStartChangeColor() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  idInteger = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR);
+}
+
+function onBtnStopChangeColor() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(idInteger);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
